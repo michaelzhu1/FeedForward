9a08cb8ddc2554fd00eae3601fb46c8499c425d4
Zhu, Zhengyan <zzhu377@gatech.edu>
2023-10-23 15:29:01 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = expresss();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
